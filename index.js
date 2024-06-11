aed7ec1de9c4e4458a8ce5edf680827cb535b41a
kayitesic250 <kayitesichartine33@gmail.com>
2024-06-11 10:25:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -69,9 +69,10 @@ app.post('/ussd', (req, res) => {
     // Determine next action based on user input
     if (userInput.length === 1 && userInput[0] === '') {
         // First level menu: Language selection
-        response = `CON Welcome to Mayor voting booth\n`;
+        response = `CON Welcome to Chartine voting booth\n`;
         response += `1. English\n`;
         response += `2. Kinyarwanda`;
+        
     } else if (userInput.length === 1 && userInput[0] !== '') {
         // Save user's language choice and move to the name input menu
         userLanguages[phoneNumber] = userInput[0] === '1' ? 'en' : 'rw';
